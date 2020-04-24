fa82689c974d722c8b617eaf28b3be617491a258
ladeiacosta <gabriel.ladeia92@gmail.com>
2020-04-24 10:23:48 -0300

    Criado tabela para trabalhar com eventos na agenda

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from core.models import Evento
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_event', 'hora_criacao')
+    list_filter = ('usuario', 'titulo', 'data_event',)
+# Essa classe diz o que vai ser mostrado no display do django no navegador, porém ela tem que ser associada no register abaixo
+
+admin.site.register(Evento, EventoAdmin)
+
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_event = models.DateTimeField()
+    hora_criacao = models.DateTimeField(auto_now=True)
+    # Parâmetro auto_now faz a variável receber a hora de criação do objeto
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    class Meta:
+        db_table = 'evento'
+
+# Meta dados da classe setam parâmetros na migração para o banco de dados
+    def __str__(self):
+        return self.titulo
+# Traz o nome do título do evento como nome do objeto
+
